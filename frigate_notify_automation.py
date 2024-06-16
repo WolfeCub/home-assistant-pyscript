067b3001dacc974d9c43ea66b4f68a96acb2d8a2
Joshua Wolfe <joshuafwolfe@gmail.com>
2024-06-16 18:08:12 +0000

    Add check to confirm clip is there before sending & small refactor.

--- frigate_notify_automation.py
+++ frigate_notify_automation.py
@@ -1,10 +1,16 @@
 import time
+import functools
 from datetime import timedelta, datetime
+
+import aiohttp
 
 event_states = {}
 
 FRIGATE_SNOOZE_ACTION = 'FRIGATE_SNOOZE'
 HASS_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+def invalid_zones(payload) -> bool:
+  return payload['camera'] == 'frigate_driveway' and 'driveway' not in payload['entered_zones']
 
 @mqtt_trigger('frigate/events', 'payload_obj["type"] == "new"')
 def frigate_new_event(payload_obj=None):
@@ -25,7 +31,7 @@ def frigate_update_event(payload_obj=None):
     log.warning(f"Frigate out of order update for id {frigate_id}")
     return
 
-  if payload_obj['after']['camera'] == 'frigate_driveway' and 'driveway' not in payload_obj['after']['entered_zones']:
+  if invalid_zones(payload_obj['after']):
     return
 
   if payload_obj['after']['has_snapshot']:
@@ -38,6 +44,13 @@ def frigate_end_event(payload_obj=None):
   log.debug(f"Frigate END event fired {payload_obj}")
   frigate_id = payload_obj['after']['id']
 
+  del event_states[frigate_id]
+  if len(event_states) > 0:
+    log.warning(f"Frigate {len(event_states)} events pending. This should be 0.")
+
+  if invalid_zones(payload_obj['after']):
+    return
+
   if payload_obj['after']['has_clip']:
     send_time = datetime.fromtimestamp(payload_obj['after']['start_time']) + timedelta(seconds=13)
     now = datetime.now()
@@ -45,12 +58,15 @@ def frigate_end_event(payload_obj=None):
       remaining_wait = send_time - now
       task.sleep(min(remaining_wait.seconds, 13)) # Likely don't need min just a safety backup
 
+    log.debug(f"Waiting till clip exists for {frigate_id}")
+    def predicate():
+      return clip_exists(frigate_id)
+    success = sleep_until(predicate)
+    if not success:
+      log.warning(f"Unable to confirm clip exists for {frigate_id}")
+
     log.debug(f"Frigate send clip update for {frigate_id}")
     send_clip(payload_obj)
-
-  del event_states[frigate_id]
-  if len(event_states) > 0:
-    log.warning(f"Frigate {len(event_states)} events pending. This should be 0.")
 
 
 @event_trigger('mobile_app_notification_action', f'action=="{FRIGATE_SNOOZE_ACTION}"')
@@ -136,3 +152,18 @@ def is_snoozed():
 
   parsed_datetime = datetime.strptime(input_datetime.frigate_notification_snooze, HASS_DATE_FORMAT)
   return datetime.now() < parsed_datetime
+
+async def clip_exists(event_id) -> bool:
+  log.info(f"Checking if clip for {event_id} exists")
+  async with aiohttp.ClientSession() as session:
+    async with session.get(make_url(event_id, 'clip.mp4')) as resp:
+      return resp.status == 200
+  return False
+
+def sleep_until(predicate: Callable[[], bool], max_tries: int=3) -> bool:
+  if max_tries <= 1:
+    return False
+  if predicate():
+    return True
+  task.sleep(5)
+  return sleep_until(predicate, max_tries-1)
